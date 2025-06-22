94047ef8de83055feec0c4db2ffbe4d18eb8c9e1
Adheesh Ramdasi <159422085+codewithaddy13@users.noreply.github.com>
2025-06-22 11:04:14 +0530

    Update chains.py

--- app/chains.py
+++ app/chains.py
@@ -12,17 +12,7 @@ class Chain:
         self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama3-70b-8192")
 
     def extract_jobs(self, cleaned_text):
-        # prompt_extract = PromptTemplate.from_template(
-        #     """
-        #     ### SCRAPED TEXT FROM WEBSITE:
-        #     {page_data}
-        #     ### INSTRUCTION:
-        #     The scraped text is from the career's page of a website.
-        #     Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
-        #     Only return the valid JSON.
-        #     ### VALID JSON (NO PREAMBLE):
-        #     """
-        # )
+        
         prompt_extract = PromptTemplate.from_template(
          """
           ### SCRAPED TEXT FROM WEBSITE:
