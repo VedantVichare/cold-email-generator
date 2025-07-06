8f74d1fa8a93e4bdc88c05e91463106e8d6071db
VedantVichare <143418623+VedantVichare@users.noreply.github.com>
2025-07-06 23:22:14 +0530

    Update portfolio.py

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import chromadb
 import uuid
-
+from chromadb.config import Settings
 
 class Portfolio:
     def __init__(self, file_path="resource/my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
-        self.chroma_client = chromadb.PersistentClient('vectorstore')
+        self.chroma_client = chromadb.Client(Settings(chroma_api_impl="local",persist_directory="vectorstore"))
         self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
 
     def load_portfolio(self):
